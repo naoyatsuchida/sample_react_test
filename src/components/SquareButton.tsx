da5feab9390680b20cf4881490cd46f440006c10
naoyatsuchida <naoyatsuchida9@gmail.com>
2022-08-31 22:44:21 +0900

    button id 追加

--- src/components/SquareButton.tsx
+++ src/components/SquareButton.tsx
@@ -5,17 +5,18 @@ type ButtonType = {
     text: string,
     onClick: React.MouseEventHandler,
     styles?: { [key: string]:string },
-    disable?: boolean
+    disable?: boolean,
+    id?:string
 }
 
 const SquareButton = (props: ButtonType) => {
-    const { text, onClick, styles, disable } = props;
+    const { text, onClick, styles, disable ,id} = props;
     let buttonStyle = {  backgroundColor: "#FFF2CC", fontWeight: "bold", color: "black" ,fontFamily: "Meiryo UI",borderRadius:"0px" ,boxShadow:"2px 2px 2px 1px rgba(0, 0, 0, 0.5)"};
     let styled = { ...buttonStyle, ...styles };
     return (
         <>
-        {disable && <Button style={styled} className='SquareButton' variant = "contained"  onClick = { onClick } disabled > { text }</Button >}
-            {!disable && <Button style={ styled} className='SquareButton' variant = "contained" onClick = { onClick }  > { text }</Button >}
+            {disable && <Button style={styled} id={id&&id } className='SquareButton' variant = "contained"  onClick = { onClick } disabled > { text }</Button >}
+            {!disable && <Button style={ styled} id={id&&id } className='SquareButton' variant = "contained" onClick = { onClick }  > { text }</Button >}
         </>
   )
 }
